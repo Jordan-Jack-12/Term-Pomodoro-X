bab4663a1a94772bf7e4bcb2734d6992b6f4fa49
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2023-07-28 10:54:00 +0530

    added csv file for storing data and some ascii art

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import os
+import csv
+from datetime import date
 
 # Clear screen
 os.system('cls')
@@ -8,36 +10,70 @@ os.system('cls')
 is_work = False
 is_rest = False
 current_interval = 1
-print(" Focus time")
-set_hh_work = int(input("focus (hrs): "))
-set_mm_work = int(input("focus (min): "))
-set_ss_work = int(input("focus (sec): "))
-os.system('cls')
-print(" short break time")
-set_mm_short_rest = int(input("short rest (min): "))
-set_ss_short_rest = int(input("short rest (sec): "))
-os.system('cls')
-print(" Long break time ")
-set_mm_long_rest = int(input("long rest (min): "))
-set_ss_long_rest = int(input("long rest (sec): "))
-os.system('cls')
-print("total interval")
-set_interval_total = int(input("number of interval : "))
-os.system('cls')
-print("long break interval")
-set_long_break_interval = int(input("long break after ___ interval : "))
-os.system('cls')
-print("\/ Confirm the start \/")
-confirm = input("Start (y/n) :")
-if confirm == 'y':
-    is_work = True
-os.system('cls')
 
-interval_total = 2*set_interval_total - 1
+# Welcom Text with aksing for inputs
+def print_welcome_text():
+    print(r"""
+        
+    __________                         .___                    ____  ___
+    \______   \____   _____   ____   __| _/___________  ____   \   \/  /
+    |     ___/  _ \ /     \ /  _ \ / __ |/  _ \_  __ \/  _ \   \     / 
+    |    |  (  <_> )  Y Y  (  <_> ) /_/ (  <_> )  | \(  <_> )  /     \ 
+    |____|   \____/|__|_|  /\____/\____ |\____/|__|   \____/  /___/\  \
+                        \/            \/                           \_/
 
+    """)
+    print("\x1b[1;31;40mNote : This is strict pomodoro you can not pause once timer starts.\x1b[0m")
+
+# Asking the input for focus time, rest time and interval    
+def input_time_interval():
+    print("Focus time")
+    set_hh_work = int(input("focus (hrs): "))
+    set_mm_work = int(input("focus (min): "))
+    set_ss_work = int(input("focus (sec): "))
+    os.system('cls')
+    print(" short break time")
+    set_mm_short_rest = int(input("short rest (min): "))
+    set_ss_short_rest = int(input("short rest (sec): "))
+    os.system('cls')
+    print(" Long break time ")
+    set_mm_long_rest = int(input("long rest (min): "))
+    set_ss_long_rest = int(input("long rest (sec): "))
+    os.system('cls')
+    print("total interval")
+    set_interval_total = int(input("number of interval : "))
+    os.system('cls')
+    print("long break interval")
+    set_long_break_interval = int(input("long break after ___ interval : "))
+    os.system('cls')
+    print("\/ Confirm the start \/")
+    confirm = input("Start (y/n) :")
+    if confirm == 'y':
+        is_work = True
+    os.system('cls')
+
+    interval_total = 2*set_interval_total - 1
+
+# Print time in proper format
 def print_time(sec):
     return time.strftime('%H:%M:%S', time.gmtime(sec))
 
+# write to csv file with date and focused time
+def save_session():
+    today_session_seconds = (set_hh_work*3600 + set_mm_work*60 + set_ss_work) * current_interval
+    time_proper_format = time.strftime('%H:%M:%S', time.gmtime(today_session_seconds))
+    today_date = str(date.today())
+    write_session = [today_date, time_proper_format]
+    if os.path.isfile('database.csv'):
+        with open('database.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(write_session)
+    else:
+        pass
+
+# App start from here
+print_welcome_text()
+input_time_interval()
 
 while interval_total > 0:
     # initializing values from input
@@ -56,11 +92,12 @@ while interval_total > 0:
             os.system('cls')
         is_work = False
         is_rest = True
+        current_interval += 1
         confirm_rest = input("Start rest (y/n): ")
         if confirm_rest == 'y':
             pass
         else:
-            break
+            save_session()
     elif is_work == False and is_rest == True:
         if long_break_here == 0:
             while time_long_rest > 0:
@@ -86,11 +123,23 @@ while interval_total > 0:
         if confirm_focus == 'y':
             pass
         else:
-            break
+            save_session()
     interval_total -= 1
-    current_interval += 1
 
-    
+# Saving session in csv file
+save_session()
+
+# Asking for exit or stay
+def conf_exit():
+    confirm_exit = input("Do you want to exit? (y/n) : ")
+    if confirm_exit == 'y' or conf_exit == 'Y':
+        break
+    elif confirm_exit == 'n' or conf_exit == 'N':
+        print_welcome_text()
+    else:
+        conf_exit()
+
+conf_exit()
 
 
 
